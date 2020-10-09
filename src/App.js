4fba8e04056ab6525d230b6988df5e844a18454c
eddie <lobros@live.de>
2020-10-09 11:29:14 +0200

    removed logo, added gradient animation for bg

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
   return (
     <div className="d-flex flex-column align-items-center justify-content-center main-bg">
       <h1 className="main-header">soon.</h1>
-      <h4 className="logo">eddie</h4>
     </div>
   );
 }
