e223b982f61c1a625193e6e67bf6fd1e4a5ab088
eddie <lobros@live.de>
2020-10-09 11:20:49 +0200

    removed scribble, added "logo" below h1, changed font to syne

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
-import Scribble from "../public/scribble.svg";
 
 function App() {
   const [date, setDate] = useState(null);
@@ -14,9 +13,9 @@ function App() {
     getDate();
   }, []);
   return (
-    <div className="d-flex align-items-center justify-content-center main-bg">
+    <div className="d-flex flex-column align-items-center justify-content-center main-bg">
       <h1 className="main-header">soon.</h1>
-      <Scribble />
+      <h4 className="logo">eddie</h4>
     </div>
   );
 }
