e395819c8925059e7d2a2e44e686323aacb2cfd5
eddie <lobros@live.de>
2020-10-13 14:01:31 +0200

    added react start script, started building header from mockups

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
+      const res = await fetch("/api/date");
       const newDate = await res.text();
       setDate(newDate);
     }
     getDate();
   }, []);
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center main-bg">
-      <h1 className="main-header">soon.</h1>
+    <div className="d-flex flex-column align-items-start justify-content-center main-bg">
+      <div className="d-flex flex-column align-items-start justify-content-center">
+        <h1 className="main-header">Hey</h1>
+        <h1 className="main-header">I’m Eddie!</h1>
+      </div>
+      {/*
+      <div className="d-flex flex-column align-items-start justify-content-center">
+        <p className="">Software Developer Apprentice <a>@Cornerstone</a> (formerly Lumesse)
+</p>
+        <p className="">Freelance UI / UX Designer <a>@whitespace_</a></p>
+      </div>
+      */}
     </div>
   );
 }
